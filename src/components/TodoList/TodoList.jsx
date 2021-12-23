0c4759c0d6978e5ff2db15e5964e6cc746af6a26
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2021-12-23 13:52:42 -0300

    update todo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,28 +3,60 @@ import React, { Component } from 'react';
 import './style.scss';
 
 export class TodoList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tasks: [],
+      prevId: 0,
+      inputTxt: '',
+    }
+    this.handleAddTask = this.handleAddTask.bind(this);
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+  handleText = (e) => {
+    this.setState((_state, _props) => ({
+      inputTxt: e.target.value,
+    }))
+  }
+
+  handleAddTask = () => {
+    this.setState((state, _props) => ({
+      tasks: [...state.tasks, {
+        id: state.prevId + 1,
+        text: state.inputTxt,
+        isCompleted: false,
+      }],
+      prevId: state.prevId + 1,
+      inputTxt: '',
+    }))
+  }
+
   render() {
     return (
       <div className="todo-list">
         <header className="input-container">
           <h1>Minhas Tasks</h1>
           <div className="inputs">
-            <input type="text" placeholder="Adicionar nova tarefa" />
-            <button>
-              <i class="far fa-check-square"></i>
+            <input onChange={this.handleText} value={this.state.inputTxt}
+              type="text" placeholder="Adicionar nova tarefa" />
+            <button onClick={this.handleAddTask}>
+              <i className="far fa-check-square"></i>
             </button>
           </div>
         </header>
         <ul className="list">
-          <li>
-            <div className="isCompleted" data-task="task">
-              <input type="checkbox" />
-              <p>placeholder task</p>
-            </div>
-            <button>
-              <i class="far fa-trash-alt"></i>
-            </button>
-          </li>
+            {this.state.tasks.map((task) => (
+              <li key={task.id}>
+                <div className={task.isCompleted? 'isCompleted': ''} data-task="task">
+                  <input type="checkbox" />
+                  <p>{task.text}</p>
+                </div>
+                <button>
+                  <i className="far fa-trash-alt"></i>
+                </button>
+              </li>
+            ))}
         </ul>
       </div>
     );
